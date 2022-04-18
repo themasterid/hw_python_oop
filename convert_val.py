91ea28243ef1a8eeb6a622fd14cde25e633abddd
Dmitriy <77408415+themasterid@users.noreply.github.com>
2022-04-18 15:45:17 +0500

    Update convert_val.py

--- convert_val.py
+++ convert_val.py
@@ -15,13 +15,11 @@ def get_rates(cur_from, date, requests):
     rates = {i.CharCode.string: (
         Decimal(i.Value.string.replace(',', '.')),
         int(i.Nominal.string)
-    ) for i in soup('Valute')
-    }
+    ) for i in soup('Valute')}
     return rates[cur_from][0]
 
 
 today = dt.date.today().strftime("%d.%m.%Y")
-print(today)
 money_from = 'USD'  # ! какой валюты
 # date_any = '2021-08-09'  # ! на какое число. 07.08.2021
 print(get_rates(money_from, today, requests), money_from)
